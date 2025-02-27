103aca698b0152cc11fe1d63cc744af8cb97ef56
Dario Quispe <106852390+Minkaspr@users.noreply.github.com>
2025-02-27 08:30:51 -0500

    build > start

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
+const AUTHOR = process.env.AUTHOR || "Desconocido";
 app.get("/", (req, res) => {
     res.json({
-        message: "Bienvenido to the API",
+        message: "Bienvenido a la API, elaborado por " + AUTHOR,
         status: "running",
         timestamp: new Date().toISOString(),
     });
